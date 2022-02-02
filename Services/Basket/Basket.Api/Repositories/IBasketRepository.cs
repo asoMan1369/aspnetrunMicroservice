4ca8eec4b1f1ab424640d11a41edcfce551055ad
asoMan <asoMan@DESKTOP-4R9EHU7>
2022-02-02 05:15:11 -0800

    basket developed

---
+++ Services/Basket/Basket.Api/Repositories/IBasketRepository.cs
@@ -0,0 +1,44 @@
+﻿using Basket.Api.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.Api.Repositories
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetBasket(string userName);
+        Task<ShoppingCart> UpdateBasket(ShoppingCart basket);
+        Task DeleteBasket(string usesrName);
+    }
+
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache _rediCache;
+        public BasketRepository(IDistributedCache distributedCache)
+        {
+            this._rediCache = distributedCache;
+        }
+        public async Task DeleteBasket(string usesrName)
+        {
+            await _rediCache.RemoveAsync(usesrName);
+        }
+
+        public async Task<ShoppingCart> GetBasket(string userName)
+        {
+            var item = await _rediCache.GetStringAsync(userName);
+            if (String.IsNullOrEmpty(item))
+                return null;
+            return JsonConvert.DeserializeObject<ShoppingCart>(item);
+        }
+
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart basket)
+        {
+            await _rediCache.SetStringAsync(basket.UserName, JsonConvert.SerializeObject(basket));
+            return await GetBasket(basket.UserName);
+        }
+    }
+}
